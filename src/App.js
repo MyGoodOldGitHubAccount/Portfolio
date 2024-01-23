13be2d33a6cc4319162d90838c0ef68d9b597d31
erewerere <Erewerere@yandex.ru>
2024-01-23 11:29:38 +0300

    Uploading project to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import sun from "./img/icons/sun.svg";
+import moon from "./img/icons/moon.svg";
+import "./css/main.css";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header"
+import Main from "./Components/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar></Navbar>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+    
+
+   
+
+    <script src="./js/main.js"></script>
+
     </div>
   );
 }
